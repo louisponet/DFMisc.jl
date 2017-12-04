b2ddf4f78014d08c24fc1fef7c37f619bd4a808e
Unknown <louisponet@gmail.com>
2017-12-04 08:09:05 +0100

    sync

--- scripts/HfO2.jl
+++ scripts/HfO2.jl
@@ -1,11 +1,20 @@
 using DFControl
-set_default_pseudo_dir(:pbesolrel,"pseudos/pbesolrel/PSEUDOPOTENTIALS/")
-set_default_pseudo_dir(:pbesol,"pseudos/pbesol/PSEUDOPOTENTIALS/")
+set_default_pseudo_dir(:pbesolrel,"/home/ponet/pseudos/pbesolrel/PSEUDOPOTENTIALS/")
+set_default_pseudo_dir(:pbesol,"/home/ponet/pseudos/pbesol/PSEUDOPOTENTIALS/")
+set_default_pseudo_dir(:sssp,"/home/ponet/pseudos/sssp/")
 configure_default_pseudos()
-HfO2 = create_job("HfO2",phd_dir*"HfO2/NSOC",server_dir = "HfO2/NSOC")
-
-pull_file("GeTe_2/rel/GeTe_scf.in",phd_dir*"GeTe/fullrel/")
-scf_calc = read_qe_input(phd_dir*"GeTe/fullrel/GeTe_scf.in")
-scf_calc.name 
-change_atoms!(scf_calc,Dict(:Hf => [Point3D{Float32}(0.9668,0.7337,0.1231)],:O=>[Point3D{Float32}(0.1350,0.0666,0.2672),Point3D{Float32}(0.7288,0.4633,0.8744)]),pseudo_set = :pbesolrel,pseudo_fuzzy="paw")
-set_default_server("ponet@10.255.9.115")
+set_default_input(:scf,read_qe_input(DFControl.assets_dir*"inputs/qe/scf.in",run_command="mpirun -np 24 ~/bin/pw.x"))
+set_default_input(:bands,read_qe_input(DFControl.assets_dir*"inputs/qe/bands.in",run_command="mpirun -np 24 ~/bin/pw.x"))
+set_default_input(:nscf,read_qe_input(DFControl.assets_dir*"inputs/qe/nscf.in",run_command="mpirun -np 24 ~/bin/pw.x"))
+default_inputs[:scf]
+atoms = get_data(HfO2,"scf",:atomic_positions)
+change_atoms!(HfO2,atoms,pseudo_set=:sssp)
+HfO2 = create_job("HfO2",phd_dir*"HfO2/NSOC",default_inputs[:scf],default_inputs[:bands],default_inputs[:projwfc],default_inputs[:nscf],server_dir = "HfO2/NSOC")
+HfO2 = load_job(phd_dir*"HfO2/NSOC",server_dir="HfO2/NSOC")
+pull_outputs(HfO2)
+using Plots
+bands = read_qe_bands_file(HfO2.local_dir*"/bands.out")
+plot(read_qe_bands_file(HfO2.local_dir*"/bands.out"),fermi=read_fermi_from_qe_file(HfO2.local_dir*"scf.out"),ylims=[-2,2],legend=false)
+remove_flags!(HfO2,:nosym)
+submit_job(HfO2)
+change_flow!(HfO2,[("nscf",false)])
